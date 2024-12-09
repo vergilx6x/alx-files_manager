a194e769cd7d15b3fd0863d7343eba4b36341c0e
vergilx6x <med.work96@hotmail.com>
2024-12-09 18:26:57 +0100

    Setting the env

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# alx-files_manager
+# 0x04. Files manager 
---
+++ utils/redis.js
@@ -0,0 +1,58 @@
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
+/**
+ * Represents a Redis client.
+ */
+class RedisClient {
+  /**
+   * Creates a new RedisClient instance.
+   */
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  /**
+   * Checks if this client's connection to the Redis server is active.
+   * @returns {boolean}
+   */
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  /**
+   * Retrieves the value of a given key.
+   * @returns {String | Object}
+   */
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  /**
+   * Stores a key and its value along with an expiration time.
+   * @returns {Promise<void>}
+   */
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
+  }
+
+  /**
+   * Removes the value of a given key.
+   * @returns {Promise<void>}
+   */
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
