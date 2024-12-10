173c1bdd2f4e0f0bdaa7c90809f1de5ee6eb48e9
vergilx6x <med.work96@hotmail.com>
2024-12-10 18:32:36 +0100

    Controllers

---
+++ middlewares/auth.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+import { Request, Response, NextFunction } from 'express';
+import { getUserFromXToken, getUserFromAuthorization } from '../utils/auth';
+
+/**
+ * Applies Basic authentication to a route.
+ */
+export const basicAuthenticate = async (req, res, next) => {
+  const user = await getUserFromAuthorization(req);
+
+  if (!user) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+  req.user = user;
+  next();
+};
+
+/**
+ * Applies X-Token authentication to a route.
+ */
+export const xTokenAuthenticate = async (req, res, next) => {
+  const user = await getUserFromXToken(req);
+
+  if (!user) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+  req.user = user;
+  next();
+};
